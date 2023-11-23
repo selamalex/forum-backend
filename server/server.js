a124f822936ab56346df911b76009a20e9cebdec
selamalex <121111132+selamalex@users.noreply.github.com>
2023-11-23 22:11:32 +0300

    new changes change here too

--- server/server.js
+++ server/server.js
@@ -1,7 +1,7 @@
 const http= require('http');
 const app=require('./index');
 
-const port= process.env.port || 5500;
+const port= process.env.port || 5000;
 const server= http.createServer(app);
 server.listen(port,()=> {
     console.log(`Started on port ${port}`);
